2d0ba4a198d433db7c3f1595c0588722adea41dc
Shane Taylor <shaneallantaylor@gmail.com>
2015-11-22 16:33:10 -0800

    changed the colors and minimized the design

--- js/main.js
+++ js/main.js
@@ -1,143 +1,35 @@
 
-$('.sample-1').click(function(e){
+$('.sample-1').mouseover(function() {
   var self = this
-  if ($(e.target).is('a')) {
-  }
-  else {
-    if (!$(".sample-1").hasClass("active")) {
-      console.log("sample-1 doesn't have the active class");
-      $(".sample-1").addClass("active");
-      $(".sample-2").addClass("unfocused");
-      $(".sample-3").addClass("unfocused");
-      $(".sample-4").addClass("unfocused");
-      $('.sample-1 .sample-title').css("height", "80px");
-      $('.sample-1 p').css("bottom", "10px");
-      $('.sample-title-1').css("margin-bottom", "4px")
-      $('.expo-container').removeClass("nope");
-      $('.arrow-up.left').removeClass("nope");
-      $('.expo-text.1').removeClass("hidden");
-      $('.expo-text.1').removeClass("nope");
-    }
-    else {
-      console.log("sample-1 DOES have the active class");
-      $(".sample-1").removeClass("active");
-      $(".sample-2").removeClass("unfocused");
-      $(".sample-3").removeClass("unfocused");
-      $(".sample-4").removeClass("unfocused");
-      $('.sample-1 .sample-title').css("height", "50px");
-      $('.sample-1 p').css("bottom", "0px");
-      $('.sample-title-1').css("margin-bottom", "8px")
-      $('.expo-container').addClass("nope");
-      $('.arrow-up.left').addClass("nope");
-      $('.expo-text.1').addClass("nope");
-      $('.expo-text.1').addClass("hidden");
-    }
-  }
+  $('.sample-1 .sample-title').css("height", "80px");
 });
 
-$('.sample-2').click(function(e){
-  if ($(e.target).is('a')) {
-  }
-  else {
-    if (!$(".sample-2").hasClass("active")) {
-      console.log("sample-2 doesn't have the active class");
-      $(".sample-2").addClass("active");
-      $(".sample-1").addClass("unfocused");
-      $(".sample-3").addClass("unfocused");
-      $(".sample-4").addClass("unfocused");
-      $('.sample-2 .sample-title').css("height", "80px");
-      $('.sample-2 p').css("bottom", "10px");
-      $('.sample-title-2').css("margin-bottom", "4px")
-      $('.expo-container').removeClass("nope");
-      $('.arrow-up.right').removeClass("nope");
-      $('.expo-text.2').removeClass("hidden");
-      $('.expo-text.2').removeClass("nope");
-    }
-    else {
-      console.log("sample-2 DOES have the active class");
-      $(".sample-2").removeClass("active");
-      $(".sample-1").removeClass("unfocused");
-      $(".sample-3").removeClass("unfocused");
-      $(".sample-4").removeClass("unfocused");
-      $('.sample-2 .sample-title').css("height", "50px");
-      $('.sample-2 p').css("bottom", "0px");
-      $('.sample-title-2').css("margin-bottom", "8px")
-      $('.expo-container').addClass("nope");
-      $('.arrow-up.right').addClass("nope");
-      $('.expo-text.2').addClass("nope");
-      $('.expo-text.2').addClass("hidden");
-    }
-  }
+$('.sample-1').mouseout(function() {
+  $('.sample-1 .sample-title').css("height", "50px");
 });
 
-$('.sample-3').click(function(e){
-  if ($(e.target).is('a')) {
-  }
-  else {
-    if (!$(".sample-3").hasClass("active")) {
-      console.log("sample-3 doesn't have the active class");
-      $(".sample-3").addClass("active");
-      $(".sample-2").addClass("unfocused");
-      $(".sample-1").addClass("unfocused");
-      $(".sample-4").addClass("unfocused");
-      $('.sample-3 .sample-title').css("height", "80px");
-      $('.sample-3 p').css("bottom", "10px");
-      $('.sample-title-3').css("margin-bottom", "4px")
-      $('.expo-container').removeClass("nope");
-      $('.arrow-up.right').removeClass("nope");
-      $('.expo-text.3').removeClass("hidden");
-      $('.expo-text.3').removeClass("nope");
-    }
-    else {
-      console.log("sample-2 DOES have the active class");
-      $(".sample-3").removeClass("active");
-      $(".sample-2").removeClass("unfocused");
-      $(".sample-1").removeClass("unfocused");
-      $(".sample-4").removeClass("unfocused");
-      $('.sample-3 .sample-title').css("height", "50px");
-      $('.sample-3 p').css("bottom", "0px");
-      $('.sample-title-3').css("margin-bottom", "8px")
-      $('.expo-container').addClass("nope");
-      $('.arrow-up.right').addClass("nope");
-      $('.expo-text.3').addClass("nope");
-      $('.expo-text.3').addClass("hidden");
-    }
-  }
+$('.sample-2').mouseover(function() {
+  $('.sample-2 .sample-title').css("height", "80px");
 });
 
-$('.sample-4').click(function(e){
-  if ($(e.target).is('a')) {
-  }
-  else {
-    if (!$(".sample-4").hasClass("active")) {
-      console.log("sample-4 doesn't have the active class");
-      $(".sample-4").addClass("active");
-      $(".sample-3").addClass("unfocused");
-      $(".sample-2").addClass("unfocused");
-      $(".sample-1").addClass("unfocused");
-      $('.sample-4 .sample-title').css("height", "80px");
-      $('.sample-4 p').css("bottom", "10px");
-      $('.sample-title-4').css("margin-bottom", "4px")
-      $('.expo-container').removeClass("nope");
-      $('.arrow-up.right').removeClass("nope");
-      $('.expo-text.4').removeClass("hidden");
-      $('.expo-text.4').removeClass("nope");
-    }
-    else {
-      console.log("sample-2 DOES have the active class");
-      $(".sample-4").removeClass("active");
-      $(".sample-3").removeClass("unfocused");
-      $(".sample-2").removeClass("unfocused");
-      $(".sample-1").removeClass("unfocused");
-      $('.sample-4 .sample-title').css("height", "50px");
-      $('.sample-4 p').css("bottom", "0px");
-      $('.sample-title-4').css("margin-bottom", "8px")
-      $('.expo-container').addClass("nope");
-      $('.arrow-up.right').addClass("nope");
-      $('.expo-text.4').addClass("nope");
-      $('.expo-text.4').addClass("hidden");
-    }
-  }
+$('.sample-2').mouseout(function() {
+  $('.sample-2 .sample-title').css("height", "50px");
+});
+
+$('.sample-3').mouseover(function() {
+  $('.sample-3 .sample-title').css("height", "80px");
+});
+
+$('.sample-3').mouseout(function() {
+  $('.sample-3 .sample-title').css("height", "50px");
+});
+
+$('.sample-4').mouseover(function() {
+  $('.sample-4 .sample-title').css("height", "80px");
+});
+
+$('.sample-4').mouseout(function() {
+  $('.sample-4 .sample-title').css("height", "50px");
 });
 
 // $('.sample-1').mouseover(function(){
